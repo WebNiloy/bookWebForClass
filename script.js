ac805290520853580c42a8f9b7a7688061e06e50
WebNiloy <niloyDev2004@gmail.com>
2024-08-18 22:26:51 +0600

    login page design

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+let loginForm = document.querySelector('.login-form-container');
+
+document.querySelector('#login-btn').onclick = () =>{
+  loginForm.classList.toggle('active');
+}
+
+document.querySelector('#close-login-btn').onclick = () =>{
+  loginForm.classList.remove('active');
+}
+
 function loader() {
     document.querySelector('.loader-container').classList.add('fade-out');
 }
